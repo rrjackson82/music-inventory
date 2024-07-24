7281b789b2159aae5544111ddcdfc6b9fba8a28c
Dane Ohr <danonator821@gmail.com>
2024-07-24 13:32:22 -0400

    album search functionality

--- web/app.js
+++ web/app.js
@@ -3,22 +3,43 @@ const placeholder = document.querySelector("#placeholder");
 const albumSearchText = document.querySelector("#album_search")
 let searchButton = document.querySelector("#searchButton")
 
-searchButton.addEventListener("click", searchAlbum)
+searchButton.addEventListener("click", () => {
+    searchAlbum(albumSearchText.value).then(data => {
+        console.log(data);
+        placeholder.textContent = `${data.title} (${data.band}) has ${(data.tracks).length} tracks`
+        albumSearchText.value = ""
+        albumSearchText.blur();
+    })
+})
+
+albumSearchText.addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        searchAlbum(albumSearchText.value).then(data => {
+            console.log(data);
+            placeholder.textContent = `${data.title} (${data.band}) has ${(data.tracks).length} tracks`
+            albumSearchText.value = ""
+            albumSearchText.blur();
+        })
+    }
+})
+
+let allSongs = {}
 
 
-async function searchAlbum() {
+async function searchAlbum(searchParams = "") {
     try {
-        const response = await fetch(`http://127.0.0.1:5000/find-album/${albumSearchText.value}`);
+        const response = await fetch(`http://127.0.0.1:5000/find-album/${searchParams}`);
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json()
-        console.log(data)
-        placeholder.textContent = `${data.title} by ${data.band} has ${(data.tracks).length} tracks`
-        albumSearchText.value = ""
+        return data
     } catch (error) {
         console.error('Error fetching data:', error);
+        alert("Search Invalid")
+        albumSearchText.value = ""
     }
 }
 
@@ -28,21 +49,21 @@ async function fetchData() {
 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
+
         }
         const data = await response.json()
         return data
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error.message);
+
+        alert("search invalid");
     }
 }
 
-// fetchData().then(data => {
-//     if (data) {
-//         let output = ""
-//         console.log(data);
-//         data.forEach(album => {
-//             output = output + " " + album.title
-//         })
-//         placeholder.textContent = output
-//     }
-// })
+fetchData().then(data => {
+    if (data) {
+        allSongs = data
+        // console.log("songs: " + songs)
+        // console.log(songs[0].title)
+    }
+})
